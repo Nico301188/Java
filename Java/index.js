b201ccedf5166767902d3a47546856c50b2aee1c
Nico301188 <nico301188@gmail.com>
2023-11-04 14:42:09 -0300

    prueba

--- Java/index.js
+++ Java/index.js
@@ -10,7 +10,7 @@ let numero = 4;
 // Las variables "let" solo se va a poder acceder dentro de un determinado
 // bloque de codigo(funciones) y NO se podra redeclarar pero si cambiar su valor
 const bool = false;
-// bool = numero
+// bool = numerox
 
 console.log("hello world!!")
 console.log(cadena)
